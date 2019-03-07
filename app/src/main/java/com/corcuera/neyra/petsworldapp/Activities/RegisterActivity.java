9b5c0ad6efa766f1090b26a25411b1b5c4063f5f
LeonardoNeyra <lneyrao1@upao.edu.pe>
2019-03-07 17:43:37 -0500

    Proyecto Inicial

---
+++ app/src/main/java/com/corcuera/neyra/petsworldapp/Activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.corcuera.neyra.petsworldapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.corcuera.neyra.petsworldapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
